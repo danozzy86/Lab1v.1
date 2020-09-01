3be42f1587e6f2b7bdab7bd286002a39c622a579
danozz86 <danozzy86@gmail.com>
2020-09-01 16:12:42 -0400

    dlo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1v._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
